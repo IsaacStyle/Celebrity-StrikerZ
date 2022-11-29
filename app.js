0228c310772b030daf74fbc6323dbf981b2a65dd
IsaacStyle <isaac.opra@gmail.com>
2022-11-29 11:48:26 -0500

    Completed card stat class

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 /*----- constants -----*/
 class Card {
-    constructor(name, atk, def, sp, ability) {
+    constructor(name, atk, def, sp, accuracy, dodge, ability, abilityText) {
         this.name = name
         this.atk = atk
         this.def = def
         this.spcost = sp
+        this.accuracy = accuracy
+        this.dodge = dodge
         this.ability = ability
+        this.abilityText = abilityText
     }
+    takeDamage(num) {
+        return this.def -= num
+    }
+    healDamage(num) {
+        return this.def += num
+    }
+    gainAttack(num) {
+        return this.atk += num
+    }
+    loseAttack(num) {
+        return this.atk -= num
+    }
+    changeAccuracy(num) {
+        this.accuracy -= num
+    }
+    changeDodge(num) {
+        this.dodge += num
+    }
+
 }
-const cardib = new Card("Cardi B.",4,5,10,true)
+const cardib = new Card("Cardi B.",4,5,10,100,0,false,"No ability")
 console.log(cardib)
-cardib.fire = true
-console.log(cardib)
+
 
   /*----- state variables -----*/
-
+let player1 = 30
+let player2 = 30
+let sp1 = 20
+let sp2 = 20
 
   /*----- cached elements  -----*/
 
